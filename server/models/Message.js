19b551e0297fd05e6b9b04a5909c6f7d0d1d900f
sgujuluva <sanghee04@gmail.com>
2022-10-20 18:13:57 +0200

    message schema created

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,6 +1,26 @@
 import mongoose from "mongoose";
 
-const MessageSchema = new mongoose.Schema({});
+const MessageSchema = new mongoose.Schema({
+  user_id: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  dates: {
+    created: Date,
+    default: Date.now(),
+    last_edited: Date,
+  },
+  category: {
+    type: String,
+    enum: ["save", "delete","post"],
+  },
+  deleted: boolean,
+});
 
 const Message = mongoose.model("message", MessageSchema);
 
