e1af05cf202d1b0d5d3cc07d0ccfad3b086a65b9
sgujuluva <sanghee04@gmail.com>
2022-10-20 13:39:54 +0200

    registeruser controller created

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,7 +1,18 @@
 import User from "../models/User.js";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 
-export const registerUser = async(req,res) => {
+export const registerUser = async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password);
 
-    const hashedPassword = await bcrypt.hash(req.body.password)
-}
+  try {
+    const createUser = await User.create({
+      username: req.body.username,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      password: req.body.password,
+    });
+    return res.status(200).json({ message: "User Created" }, createUser);
+  } catch (error) {
+    return res.status(500).json({message:error.message})
+  }
+};
