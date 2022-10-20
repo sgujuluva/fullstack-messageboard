30eeb608c45457a2b0a766ff4e97634d21239e85
sgujuluva <sanghee04@gmail.com>
2022-10-20 10:58:21 +0200

    user schema done

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,18 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({});
+const UserSchema = new mongoose.Schema({
+    username: {type:String, required :true, unique:true},
+firstname: {type:String, required :true},
+lastname: String,
+ip: String,
+hash: {type:String, required :true},
+avatar: {type:String, default: generateRobohashAvatar()},
+dates: {
+    registered: {type:Date, default: Date.now()},
+    last_active: Date
+},
+messages: Number
+});
 
 const User = mongoose.model("user", UserSchema);
 
