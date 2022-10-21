4de830c835d2fbb673ec025e151f3c8173af88ae
sgujuluva <sanghee04@gmail.com>
2022-10-21 14:41:56 +0200

    login user created

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -2,25 +2,50 @@ import User from "../models/User.js";
 import bcrypt from "bcrypt";
 
 export const registerUser = async (req, res) => {
+
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-    try { 
-  const checkUserNameExist = await User.findOne({
-    username: req.body.username,
-  });
-  if (checkUserNameExist) {
-    return res.status(409).json({ message: "User already registered" });
-  } else {
-    const createUser = await User.create({
+  try {
+    const checkUserNameExist = await User.findOne({
       username: req.body.username,
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-      password: hashedPassword,
-      ip: req.ip,
     });
-    return res.status(200).json({ message: "User Created", createUser });
-  }
+    if (checkUserNameExist) {
+      return res.status(409).json({ message: "User already registered" });
+    } else {
+      const createUser = await User.create({
+        username: req.body.username,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        password: hashedPassword,
+        ip: req.ip,
+      });
+      return res.status(200).json({ message: "User Created", createUser });
+    }
   } catch {
-    return res.status(500).json({ message: "something went wrong during creation" });
-  } 
+    return res
+      .status(500)
+      .json({ message: "something went wrong during creation" });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  const {password} = req.body;
+
+  try{
+    if(!password){
+      return res.status(400).send("No password given, Kindly enter Password")
+    }
+    const user = User.findOne({username:username})
+    if(!user){
+      return res.status(400).send("no user found")
+    }
+
+    const checkPassword = await bcrypt.compare(password, user.password);
+
+    if(checkPassword){
+      console.log("You are authenticated")
+    }
+    //generate a token for the user
+    
+  }
 };
