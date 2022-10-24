644844947420bfe352ef683263456cfceca7d2be
sgujuluva <sanghee04@gmail.com>
2022-10-24 10:42:51 +0200

    edit message controller written

--- server/controllers/messageControllers.js
+++ server/controllers/messageControllers.js
@@ -10,3 +10,11 @@ export const messagePost = async (req, res) => {
   return res.status(200).json({ message: "message posted", postMessage });
 };
 
+export const editMessage = async(req,res) => {
+    const editingMessage = await Message.create({
+        user_id: req.body.user_id,
+        message_id: req.body.message_id,
+        content:req.body.content
+    })
+    return res.status(200).json({message:"message edited",editMessage})
+}
