a7fe7dcb29b0154d13d5f5c03718f3c7169234eb
sgujuluva <sanghee04@gmail.com>
2022-10-25 18:39:03 +0200

    handlesubmit for registration form is created

--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -1,30 +1,58 @@
-export default function Register() {
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-    };
+import axios from "axios";
+import { useState } from "react";
+import {useNavigate} from "react-router-dom"
 
-    return (
-        <>
-            <h1>Register</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    First name
-                    <input type="text" autoComplete="firstname" name="firstname" required/>
-                </label>
-                <label>
-                    Last name
-                    <input type="text" autoComplete="lastname" name="lastname" required/>
-                </label>
-                <label>
-                    E-mail
-                    <input type="email" autoComplete="email" name="email" required/>
-                </label>
-                <label>
-                    Password
-                    <input type="password" autoComplete="new-password" name="password" required/>
-                </label>
-                <button type="submit">Register</button>
-            </form>
-        </>
-    );
+export default function Register() {
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    try {
+      await axios.post("http://localhost:3001/user/createuser", {
+        firstName: formData.get("firstname"),
+        lastName: formData.get("lastname"),
+        userName: formData.get("username"),
+        email: formData.get("email"),
+        password: formData.get("password"),
+      });
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  };
+
+  return (
+    <>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          First name
+          <input
+            type="text"
+            autoComplete="firstname"
+            name="firstname"
+            required
+          />
+        </label>
+        <label>
+          Last name
+          <input type="text" autoComplete="lastname" name="lastname" required />
+        </label>
+        <label>
+          E-mail
+          <input type="email" autoComplete="email" name="email" required />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            autoComplete="new-password"
+            name="password"
+            required
+          />
+        </label>
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
 }
