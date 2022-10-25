86ac8ede72b6ca4f093438be6e4367e70652e914
sgujuluva <sanghee04@gmail.com>
2022-10-25 18:47:01 +0200

    router path set for login register and home

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Homepage from "./components/Homepage";
 import Register from './components/Register';
+import Login from "./components/Login"
+import {Routes, Route} from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
-            <Register/>
+<Routes>
+    <Route path="/register" element={<Register/>}/>
+    <Route path="/login" element={<Login/>}/>
+    <Route path="/" element={<Homepage/>}/>
+</Routes>
+          
         </div>
     );
 }
