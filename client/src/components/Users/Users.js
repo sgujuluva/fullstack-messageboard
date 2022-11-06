806ef2918359a5e4984eb7d2baf1df19eb5afa1f
sgujuluva <sanghee04@gmail.com>
2022-11-06 21:25:25 +0100

    user data populated

--- client/src/components/Users/Users.js
+++ client/src/components/Users/Users.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import axios from "axios"
 
 function Users() {
+
+  const [data,setData] = useState("");
+
+  const getUsers = async() => {
+    const response = await axios.get("http://localhost:3001/user/getAllUsers", {
+      withCredentials:true
+    })
+    if(response.data){
+      setData(response.data)
+    }
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
-    <div>Users</div>
+    <div>
+      {data && data.map(user => (
+        <ul key = {user._id}>
+          <p>{user.firstname}</p>
+          <img src={user.avatar} alt ="avatar"/>
+        </ul>
+      ))}
+    </div>
   )
 }
 
